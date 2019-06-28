123082cb1b039f60c16d67af6f29cf9569724590
Ashgomathi <51981037+Ashgomathi@users.noreply.github.com>
2019-06-28 15:31:55 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
-as2=input()
-if(as2 >='a'and as1 <='z') or (as2 >='A' and as1 <='Z'):
+as1=input()
+if(as1 >='a'and as1 <='z') or (as1 >='A' and as1 <='Z'):
   print("Alhabet")
 else:
   print("No")
